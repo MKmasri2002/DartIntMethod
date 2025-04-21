feee321435551265e19677a7cf35d8ec956ab10c
Mahmoud Almasri <25046820@std.ltuc.edu.jo>
2025-04-21 18:27:59 +0300

    update

--- main.dart
+++ main.dart
@@ -5,9 +5,9 @@ void main() {
   int age = 23;
   double myhigest = -180.1;
   print(myname[0].toUpperCase());
-  print(myhigest.ceil());
-  print(myhigest.floor());
-  print(myhigest.round());
+  print(myhigest.ceil() * 0);
+  print(myhigest.floor() * 0);
+  print(myhigest.round() * 0);
   print(myhigest.isNegative);
   print(myhigest.abs());
 }
