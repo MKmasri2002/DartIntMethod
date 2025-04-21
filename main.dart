316fba064f35c52080bc65fb43ad796ad430ca2a
Mahmoud Almasri <25046820@std.ltuc.edu.jo>
2025-04-21 18:24:33 +0300

    update

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  String myname = "Mahmood Almasri";
+  int age = 23;
+  double myhigest = -180.1;
+  print(myname[0].toUpperCase());
+  print(myhigest.ceil());
+  print(myhigest.floor());
+  print(myhigest.round());
+  print(myhigest.isNegative);
+  print(myhigest.abs());
+}
